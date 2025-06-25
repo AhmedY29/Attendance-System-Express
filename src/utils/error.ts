f2d8ec7f079178d381d89e9efd20c628c7ec0a6f
AhmedY29 <ahmedys2001@gmail.com>
2025-06-25 23:21:02 +0300

    try..

--- src/utils/error.ts
+++ src/utils/error.ts
@@ -7,6 +7,8 @@ export class AppError extends Error {
     this.statusCode = statusCode;
     this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error';
 
-    Error.captureStackTrace(this, this.constructor);
+   if (Error.captureStackTrace) {
+  Error.captureStackTrace(this, this.constructor);
+}
   }
 } 
